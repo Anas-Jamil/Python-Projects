226557ee6c1b9cd2ad88361650f1e1657231ec2a
Anas-Jamil <146596074+Anas-Jamil@users.noreply.github.com>
2023-11-03 23:34:03 -0400

    Delete mini_Projects directory

--- mini_Projects/Test.py
+++
@@ -1,8 +0,0 @@
-while True:
-    try:
-        User_Input = float(input("Hey would you like to draw a card Yes or No: "))
-        while User_Input != "Yes" or "yes" or "No" or "no":
-            User_Input = float(input("Please enter Yes or No only! "))
-        break
-    except ValueError:
-        print("Please input an integer only!")
