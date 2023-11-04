c13aedfcc097b50ae825c506a928aebcebb3a28b
Anas-Jamil <146596074+Anas-Jamil@users.noreply.github.com>
2023-11-03 23:33:46 -0400

    Delete Personal_Programs directory

--- Personal_Programs/test1.py
+++
@@ -1,8 +0,0 @@
-while True:
-    s = input("Please insert a numerical value for a: ")
-    try:
-        a = float(s)
-        break
-    except ValueError:
-        print ("Please enter a numeric value.")
-print ("Well done.")
