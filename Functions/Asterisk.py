9f8f7df6dd0900dce615925179518137c14babda
Anas-Jamil <146596074+Anas-Jamil@users.noreply.github.com>
2023-11-03 23:33:31 -0400

    Delete Functions directory

--- Functions/Asterisk.py
+++
@@ -1,7 +0,0 @@
-def display_square(size, Character):
-    for count in range(size):
-        print(Character*size)
-
-Character = input("Please enter a single character: ")
-size = int(input("Please enter a positive number: "))
-display_square(size, Character) 
--- Functions/Num_of_letters.py
+++
@@ -1,7 +0,0 @@
-
-def count_chars(txt):
-    result = 0
-    for char in txt:
-        result += 1    
-    return result
-print count_chars(text)
